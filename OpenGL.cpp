b2c27839983fe2331f2240fc8c66fa9d1e8d6b61
Adam <duonggiakhanhb@gmail.com>
2021-12-06 13:24:55 +0700

    camera

--- OpenGL.cpp
+++ OpenGL.cpp
@@ -102,11 +102,7 @@ int main() {
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
-
-    // Bảo GLFW tôi muốn input con chuột
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // load tất cả hàm của glad
     // ---------------------------------------
@@ -153,7 +149,7 @@ int main() {
 
         // input
         // -----
-        processInput(window);
+        camera.Inputs(window);
 
         // render
         // ------
@@ -181,19 +177,6 @@ int main() {
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react
 // accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera.ProcessKeyboard(FORWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera.ProcessKeyboard(BACKWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        camera.ProcessKeyboard(LEFT, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        camera.ProcessKeyboard(RIGHT, deltaTime);
-}
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
@@ -201,24 +184,6 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     // make sure the viewport matches the new window dimensions; note that width and
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
-}
-
-// glfw: whenever the mouse moves, this callback is called
-// -------------------------------------------------------
-void mouse_callback(GLFWwindow *window, double xpos, double ypos) {
-    if (firstMouse) {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos;  // reversed since y-coordinates go from bottom to top
-
-    lastX = xpos;
-    lastY = ypos;
-
-    camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
--- header/camera.h
+++ header/camera.h
@@ -5,6 +5,9 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+#include<glm/gtx/rotate_vector.hpp>
+#include<glm/gtx/vector_angle.hpp>
 #include <vector>
 
 // Defines several possible options for camera movement. Used as abstraction to stay away from
@@ -17,6 +20,10 @@ const float PITCH = 0.0f;
 const float SPEED = 2.5f;
 const float SENSITIVITY = 0.1f;
 const float ZOOM = 45.0f;
+const unsigned int width = 900;
+const unsigned int height = 900;
+float sensitivity = 100.0f;
+
 
 // An abstract camera class that processes input and calculates the corresponding Euler Angles,
 // Vectors and Matrices for use in OpenGL
@@ -35,7 +42,8 @@ public:
     float MovementSpeed;
     float MouseSensitivity;
     float Zoom;
-
+    bool firstClick;
+    void Inputs(GLFWwindow *window);
     // constructor with vectors
     Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
            glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH)
@@ -68,17 +76,17 @@ public:
 
     // processes input received from any keyboard-like input system. Accepts input parameter in the
     // form of camera defined ENUM (to abstract it from windowing systems)
-    void ProcessKeyboard(Camera_Movement direction, float deltaTime) {
-        float velocity = MovementSpeed * deltaTime;
-        if (direction == FORWARD)
-            Position += Front * velocity;
-        if (direction == BACKWARD)
-            Position -= Front * velocity;
-        if (direction == LEFT)
-            Position -= Right * velocity;
-        if (direction == RIGHT)
-            Position += Right * velocity;
-    }
+    // void ProcessKeyboard(Camera_Movement direction, float deltaTime) {
+    //     float velocity = MovementSpeed * deltaTime;
+    //     if (direction == FORWARD)
+    //         Position += Front * velocity;
+    //     if (direction == BACKWARD)
+    //         Position -= Front * velocity;
+    //     if (direction == LEFT)
+    //         Position -= Right * velocity;
+    //     if (direction == RIGHT)
+    //         Position += Right * velocity;
+    // }
 
     // processes input received from a mouse input system. Expects the offset value in both the x
     // and y direction.
@@ -136,4 +144,91 @@ private:
         Up = glm::normalize(glm::cross(Right, Front));
     }
 };
+void Camera::Inputs(GLFWwindow* window)
+{
+    float speed = 0.01f;
+    // Handles key inputs
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        Position += speed * Front;
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        Position += speed * -glm::normalize(glm::cross(Front, Up));
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        Position += speed * -Front;
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        Position += speed * glm::normalize(glm::cross(Front, Up));
+    }
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+    {
+        Position += speed * Up;
+    }
+    if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+    {
+        Position += speed * -Up;
+    }
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+    {
+        speed = 0.4f;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE)
+    {
+        speed = 0.1f;
+    }
+
+
+    // Handles mouse inputs
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+    {
+        // Hides mouse cursor
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+        // Prevents camera from jumping on the first click
+        if (firstClick)
+        {
+            glfwSetCursorPos(window, (width / 2), (height / 2));
+            firstClick = false;
+        }
+
+        // Stores the coordinates of the cursor
+        double mouseX;
+        double mouseY;
+        // Fetches the coordinates of the cursor
+        glfwGetCursorPos(window, &mouseX, &mouseY);
+
+        // Normalizes and shifts the coordinates of the cursor such that they begin in the middle of the screen
+        // and then "transforms" them into degrees 
+        float rotX = sensitivity * (float)(mouseY - (height / 2)) / height;
+        float rotY = sensitivity * (float)(mouseX - (width / 2)) / width;
+
+        // Calculates upcoming vertical change in the Front
+        glm::vec3 newFront = glm::rotate(Front, glm::radians(-rotX), glm::normalize(glm::cross(Front, Up)));
+
+        // Decides whether or not the next vertical Front is legal or not
+        if (abs(glm::angle(newFront, Up) - glm::radians(90.0f)) <= glm::radians(85.0f))
+        {
+            Front = newFront;
+        }
+
+        // Rotates the Front left and right
+        Front = glm::rotate(Front, glm::radians(-rotY), Up);
+
+        // Sets mouse cursor to the middle of the screen so that it doesn't end up roaming around
+        glfwSetCursorPos(window, (width / 2), (height / 2));
+    }
+    else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE)
+    {
+        // Unhides cursor since camera is not looking around anymore
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        // Makes sure the next time the camera looks around it doesn't jump
+        firstClick = true;
+    }
+}
 #endif
