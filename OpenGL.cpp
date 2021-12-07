5bb722de384b319636f55305aa13f3e3919841e2
Adam <duonggiakhanhb@gmail.com>
2021-12-08 01:59:42 +0700

    changed first view

--- OpenGL.cpp
+++ OpenGL.cpp
@@ -26,7 +26,8 @@ const unsigned int SCR_WIDTH = 900;
 const unsigned int SCR_HEIGHT = 690;
 
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+Camera camera(glm::vec3(0.0f, 1.5f, 6.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
