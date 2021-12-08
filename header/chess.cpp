d50a60951e487003e09cf185c43b37f1acc9f912
Phong <phong940253@gmail.com>
2021-12-08 08:43:38 +0700

    Thêm hàm tính tọa độ thực của cờ

--- header/chess.cpp
+++ header/chess.cpp
@@ -12,4 +12,9 @@ public:
     // Tính toán các tọa độ có thể đi đến
     virtual void computeAvailableMovements(std::vector<Object *> &owner,
                                            std::vector<Object *> &opponent){};
+
+    // Tính toán toạ độ thực của quân cờ
+    glm::vec3 computeRealPosition(int posX, int posY) {
+        return glm::vec3(0, 0, 0);  // TODO
+    }
 };
